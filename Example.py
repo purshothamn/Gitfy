b23a1f696a756f64ede70b0f9cc7689b06b311af
Purshoth <purshoth27@gmail.com>
2020-11-16 18:27:17 +0530

    'Refer_changelog.txt'

---
+++ Example.py
@@ -0,0 +1,27 @@
+from Gitfy import Gitfy
+
+g = Gitfy()
+
+# To initialize empty repository
+# g.gitInit()
+
+# To check the git status
+# g.gitStatus()
+
+# To Get list of New files added to repository
+# print(g.getNewFiles())
+
+# To Get list of Modified files added to repository
+# print(g.getModified())
+
+# To Add New files to staging
+# print(g.addNewFiles())
+
+# To Add Modified files to staging
+# print(g.addModifiedFiles())
+
+# To commit staged files
+# print(g.gitCommit())
+
+# To push the changes to repository
+# print(g.gitPush())
